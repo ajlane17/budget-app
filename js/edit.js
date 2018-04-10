7e47a23bcfa5a718b7eb6ee063e8ca09cfccfb68
Adrian Lane <ajlane17@yahoo.com>
2018-04-09 22:01:39 -0600

    initial commit

---
+++ js/edit.js
@@ -0,0 +1,81 @@
+/*jshint esversion: 6 */
+let transaction = sessionStorage.transaction;
+
+window.onload = function () {
+    if (transaction != "") {
+        getTransaction();
+    }
+};
+
+function reqListener (xhr) {
+    console.log(xhr.responseText);
+}
+
+function formatParams( params ){
+    return Object
+          .keys(params)
+          .map(function(key){
+            return key+"="+encodeURIComponent(params[key]);
+          })
+          .join("&");
+  }
+
+function cancelUpdate() {
+    location.href = "index.html";
+}
+
+function getTransaction() {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1:3000/storage/" + transaction);
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            reqListener(xhr);
+            loadTransaction(xhr);
+        }
+    };
+}
+
+function loadTransaction(xhr) {
+    let response = JSON.parse(xhr.responseText);
+    document.forms["editTransactionForm"]["date"].value = response.date;
+    document.forms["editTransactionForm"]["payee"].value = response.payee;
+    document.forms["editTransactionForm"]["memo"].value = response.memo;
+    document.forms["editTransactionForm"]["category"].value = response.category;
+    document.forms["editTransactionForm"]["amount"].value = response.amount;
+}
+
+function updateTransaction() {
+    let date = document.forms["editTransactionForm"]["date"].value;
+    let payee = document.forms["editTransactionForm"]["payee"].value;
+    let memo = document.forms["editTransactionForm"]["memo"].value;
+    let category = document.forms["editTransactionForm"]["category"].value;
+    let amount = document.forms["editTransactionForm"]["amount"].value;
+
+    let params = {
+        date: date, 
+        payee: payee,
+        memo: memo,
+        category: category,
+        amount: amount
+      };
+
+    let xhr = new XMLHttpRequest();
+    if (transaction != "") {
+        xhr.open("PUT", 'http://localhost:3000/storage/' + transaction, true);
+    } else {
+        xhr.open("POST", 'http://localhost:3000/storage', true);
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            alert("Transaction Saved");
+            location.href = "index.html";
+        }
+    };
+
+    xhr.send(formatParams(params));
+}
---
+++ js/main.js
@@ -0,0 +1,107 @@
+/*jshint esversion: 6 */
+
+window.onload = function () {
+    getRecords();
+};
+
+function reqListener (xhr) {
+    console.log(xhr.responseText);
+}
+
+function getRecords () {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1:3000/storage", true);
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            reqListener(xhr);
+            createLineItems(xhr);
+        }
+    };
+}
+
+function createLineItems (xhr) {
+    // TODO: document.onready
+    // create table row for each record
+
+    // get transaction data
+    let response = JSON.parse(xhr.responseText);
+    let table = document.getElementById('transactions');
+    // for each line item
+    response.forEach(function(element) {
+        // create a table row with transaction details
+        let rowCount = table.rows.length;
+        let row = table.insertRow(rowCount);
+
+        let dateCol = row.insertCell(0);
+        dateCol.innerHTML = element.date;
+        dateCol.id = element._id + "date";
+
+        let payeeCol = row.insertCell(1);
+        payeeCol.innerHTML = element.payee;
+        payeeCol.id = element._id + "payee";
+
+        let memoCol = row.insertCell(2);
+        memoCol.innerHTML = element.memo;
+        memoCol.id = element._id + "memo";
+
+        let categoryCol = row.insertCell(3);
+        categoryCol.innerHTML = element.category;
+        categoryCol.id = element._id + "category";
+
+        let amountCol = row.insertCell(4);
+        amountCol.innerHTML = element.amount;
+        amountCol.id = element._id + "amount";
+        
+        // add edit button
+        let editButton = document.createElement('button');
+        editButton.id = element._id;
+        editButton.innerHTML = "Edit";
+        editButton.onclick = function(){updateRecord(this.id);};
+        let editCol = row.insertCell(5);
+        editCol.appendChild(editButton);
+        
+        // add delete button
+        let deleteButton = document.createElement('button');
+        deleteButton.id = element._id;
+        deleteButton.innerHTML = "Delete";
+        deleteButton.onclick = function(){deleteRecord(this.id);};
+        let deleteCol = row.insertCell(6);
+        deleteCol.appendChild(deleteButton);
+    });
+}
+
+function updateRecord(id) {
+    sessionStorage.transaction = id;
+    location.href = "edit.html";
+}
+
+function createRecord() {
+    sessionStorage.transaction = "";
+    location.href = "edit.html";
+}
+
+function deleteRecord(id) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("DELETE", "http://127.0.0.1:3000/storage/" + id, true);
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            reqListener(xhr);
+            refreshTable();
+        }
+    };
+}
+
+function refreshTable() {
+    let table = document.getElementById('transactions');
+    var rows = table.rows;
+    var i = rows.length;
+    while (--i) {
+        rows[i].parentNode.removeChild(rows[i]);
+    }
+    getRecords();
+}
